cc64572077d1e826e71637f9bd77ce2d8cd37972
franCarre <56691795+franCarre@users.noreply.github.com>
2020-07-23 10:58:32 +0500

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,7 @@
+<template>
+   <div>
+      <h2>Home Page</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non aliquid libero quia magnam obcaecati sequi consequuntur ad reprehenderit aperiam. Eum, suscipit aliquam. Quod aut assumenda excepturi harum corrupti quia dolore.</p>
+      <router-link to="/todos">Todos</router-link>
+   </div>
+</template>
